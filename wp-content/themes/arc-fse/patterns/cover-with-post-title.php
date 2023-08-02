0e53324bdfae20c50aadf1717dc609038e97d25b
Karuna Shukla <karuna.omak@gmail.com>
2023-08-02 18:03:34 +0530

    Mera Code Phahla pahla nashaaa wordpress kaa

---
+++ wp-content/themes/arc-fse/patterns/cover-with-post-title.php
@@ -0,0 +1,14 @@
+<?php
+ /**
+  * Title: Cover With Post Title
+  * Slug: arc-fse/cover-with-post-title
+  * Categories: arc-fse
+  */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/cover-banner.jpg","id":143,"dimRatio":70,"overlayColor":"black","focalPoint":{"x":"0.35","y":"0.38"},"minHeight":399,"minHeightUnit":"px","align":"full","style":{"spacing":{"padding":{"top":"10vw","right":"30px","bottom":"10vw","left":"30px"}}}} -->
+<!-- wp:group {"align":"wide","layout":{"inherit":true}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":false}} -->
+<div class="wp-block-group alignwide"><!-- wp:post-title {"textAlign":"center","textColor":"foreground"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
